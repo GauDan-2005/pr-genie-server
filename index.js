5318b19e009a54d9e7025c0f79276ff25e5ec249
Gaurav Singh <99481376+GauDan-2005@users.noreply.github.com>
2024-10-15 17:06:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
     secret: process.env.SESSION_SECRET || "your-session-secret",
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true },
+    cookie: { secure: false },
     sameSite: "none",
     maxAge: 10 * 24 * 60 * 60 * 1000, // 10 day
   })
