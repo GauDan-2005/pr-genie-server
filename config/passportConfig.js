a6d43455df96726fcfa0c624c8502a443a894e5f
GauDan-2005 <gauravdan2005@gmail.com>
2024-10-15 07:43:26 +0530

    callback url fixed

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -19,7 +19,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:5173/auth/github/callback",
+      callbackURL: `${process.env.CLIENT_URL}/auth/github/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
